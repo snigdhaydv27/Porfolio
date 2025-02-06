4cf43807c4070a5b3625ecba8d3d2c169b13889d
snigdhaydv27 <snigdhak.ug23.cs@nitp.ac.in>
2025-02-06 16:16:40 +0530

    navbar nad footer done

---
+++ src/components/footer/FooterMain.jsx
@@ -0,0 +1,13 @@
+
+const FooterMain = () => {
+  return (
+    <div className="px-4">
+      <div className="w-full h-[1px] bg-lightGrey mt-24"></div>
+      <p className="max-w-[1200px] mx-auto text-right mt-4 mb-12 text-sm text-lightBrown">
+        ©2025 Snigdha | All Rights Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default FooterMain;
